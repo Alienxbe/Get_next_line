fda5dad96773ae82ac9431906974c47e865554de
Alienxbe <martin.ykman@gmail.com>
2020-12-13 20:32:55 +0100

    Starting the projetx + adding .gitignore

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,16 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 17:59:17 by mykman            #+#    #+#             */
-/*   Updated: 2020/12/12 18:04:02 by mykman           ###   ########.fr       */
+/*   Updated: 2020/12/13 20:31:43 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 int	get_next_line(int fd, char **line)
 {
+	char	*buf[BUFFER_SIZE + 1];
 	
+	if (fd < 0 || !line || read(fd, buf, BUFFER_SIZE) < 0)
+		return (-1);
 }
--- main.c
+++ main.c
@@ -6,14 +6,30 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 18:01:52 by mykman            #+#    #+#             */
-/*   Updated: 2020/12/12 18:02:13 by mykman           ###   ########.fr       */
+/*   Updated: 2020/12/13 20:31:41 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include "get_next_line.h"
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-	/* code */
-	return 0;
+	int		fd;
+	int		bytes;
+	char	buf[BUFFER_SIZE + 1];
+
+	if ((fd = open("test.txt", O_RDONLY)) < 0)
+		return (1);
+
+	bytes = read(fd, buf, BUFFER_SIZE);
+	printf("%d : %s\n", bytes, buf);
+
+
+	if ((close(fd)) < 0)
+		return (1);
+	return (0);
 }
